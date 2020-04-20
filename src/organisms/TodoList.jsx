4f18f3fc51b3594a89e0f08218779e941d43af96
HironoriSato <maza.hs5360@gmail.com>
2020-04-21 01:26:03 +0900

    [add]TodoList organismsを作成

---
+++ src/organisms/TodoList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import ListItem from '../molecules/ListItem'
+import Button from '../atoms/Button'
+import './TodoList.css'
+
+const TodoList = ({ todosFromCon, deleteTodo, toggleTodoCompleted, visibleFilter, showAll, showActive, showCompleted }) => {
+  const todos = todosFromCon.filter(todo => {
+    switch (visibleFilter) {
+      case 'SHOW_ALL':
+        return todo
+        break;
+      case 'SHOW_ACTIVE':
+        return !todo.hasCompleted()
+        break;
+      case 'SHOW_COMPLETED':
+        return todo.hasCompleted()
+        break;
+    }
+  })
+
+  const renderList = todos.map((todo, idx) => {
+    const actions = {deleteTodo, toggleTodoCompleted}
+    return (
+      <ListItem key={idx} id={idx} {...actions}>{todo.text}</ListItem>
+    )
+  })
+
+  return (
+    <div>
+      <Button clkHandler={() => showAll()}>SHOW ALL</Button>
+      <Button clkHandler={() => showActive()}>SHOW ACTIVE</Button>
+      <Button clkHandler={() => showCompleted()}>SHOW COMPLETED</Button>
+      <ul>
+        {renderList}
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList
