ad90cf2ffc5b29be53d83ca46edba41f678ec552
HironoriSato <maza.hs5360@gmail.com>
2020-04-22 20:30:29 +0900

    [mod]thunk, redux-dev-toolsをstoreに設定

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import rootReducer from '../reducers/index'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store
