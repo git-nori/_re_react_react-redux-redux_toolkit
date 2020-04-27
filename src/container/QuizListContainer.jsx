c353f90e367e26798d5a332df2dfc6f087528465
HironoriSato <maza.hs5360@gmail.com>
2020-04-28 00:20:22 +0900

    [mod]storeのquizデータをQuizListに渡すようmapStateToPropsを設定

--- src/container/QuizListContainer.jsx
+++ src/container/QuizListContainer.jsx
@@ -3,10 +3,14 @@ import { fetchQuiz } from '../actions/quizActionCreators'
 
 import QuizList from '../organisms/QuizList'
 
+const mapStateToProps = (state, ownProps) => ({
+  quizInfo: state.quiz
+})
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchQuiz: () => dispatch(fetchQuiz())
 })
 
-const QuizListContainer = connect(null, mapDispatchToProps)(QuizList)
+const QuizListContainer = connect(mapStateToProps, mapDispatchToProps)(QuizList)
 
 export default QuizListContainer
